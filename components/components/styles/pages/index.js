a057d7a7af3bb5f0feb449a946bb42582540c0e6
KatyaTam11 <91533453+KatyaTam11@users.noreply.github.com>
2025-06-20 12:14:14 +0300

    Create index.js

---
+++ components/components/styles/pages/index.js
@@ -0,0 +1,17 @@
+import Navbar from '../components/Navbar';
+import Header from '../components/Header';
+import Features from '../components/Features';
+import Integrations from '../components/Integrations';
+import Footer from '../components/Footer';
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <Features />
+      <Integrations />
+      <Footer />
+    </>
+  );
+}
